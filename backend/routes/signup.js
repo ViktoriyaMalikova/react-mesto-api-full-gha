07893d31f7c8c456bc83cfe8c430fd83c12e63c5
Viktoriya Malikova <v9229042@yandex.ru>
2024-01-15 18:58:09 +0400

    исправлены замечания ревью

--- backend/routes/signup.js
+++ backend/routes/signup.js
@@ -9,7 +9,7 @@ router.post('/', celebrate({
     about: Joi.string().min(2).max(30),
     avatar: Joi.string().regex(urlRegex),
     email: Joi.string().required().email(),
-    password: Joi.string().required().min(8),
+    password: Joi.string().required(),
   }),
 }), addUser);
 
